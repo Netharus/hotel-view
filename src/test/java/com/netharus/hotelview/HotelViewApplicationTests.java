cc7075e694eaab59f686c45cf2a58bec2d2a677d
Netharus <nekromant12331@gmail.com>
2025-07-24 13:56:49 +0300

    feat: initial commit

---
+++ src/test/java/com/netharus/hotelview/HotelViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netharus.hotelview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelViewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
