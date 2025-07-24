54e265c60e67c540433e30a70982422c12fadd10
Netharus <nekromant12331@gmail.com>
2025-07-24 20:33:11 +0300

    feat: add service layer

---
+++ src/main/java/com/netharus/hotelview/controller/HotelRestController.java
@@ -0,0 +1,10 @@
+package com.netharus.hotelview.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/property-view/hotels")
+public class HotelRestController {
+
+}
